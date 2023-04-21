206d2244a0138bb89bc7388670b6202022a06801
CesarArellano <raywayday@gmail.com>
2023-04-20 23:36:29 -0600

    Learning again to use SQL

--- Querys/Consulta_Alumnos.sql
+++ Querys/Consulta_Alumnos.sql
@@ -1,3 +1,74 @@
+/* Creación de la tabla de alumnos */
+CREATE TABLE a_Alumnos (
+  AlumnoID INT PRIMARY KEY,
+  Nombre VARCHAR(50),
+  Genero VARCHAR(10)
+);
+
+/* Inserción de algunos registros de ejemplo en la tabla de alumnos */
+INSERT INTO a_Alumnos (AlumnoID, Nombre, Genero) VALUES
+(1, 'Ana', 'Femenino'),
+(2, 'Juan', 'Masculino'),
+(3, 'María', 'Femenino'),
+(4, 'Pedro', 'Masculino'),
+(5, 'Sofía', 'Femenino'),
+(6, 'Carlos', 'Masculino');
+
+/* Creación de la tabla de examenes */
+CREATE TABLE a_Examenes (
+  ExamenID INT PRIMARY KEY,
+  Nombre VARCHAR(50)
+);
+
+/* Inserción de algunos registros de ejemplo en la tabla de examenes */
+INSERT INTO a_Examenes (ExamenID, Nombre) VALUES
+(1, 'Matemáticas'),
+(2, 'Física'),
+(3, 'Historia');
+
+/* Creación de la tabla de calificaciones */
+CREATE TABLE a_Calificaciones (
+  CalificacionID INT PRIMARY KEY,
+  AlumnoID INT,
+  ExamenID INT,
+  Calificacion FLOAT,
+  FOREIGN KEY (AlumnoID) REFERENCES a_Alumnos(AlumnoID),
+  FOREIGN KEY (ExamenID) REFERENCES a_Examenes(ExamenID)
+);
+
+/* Inserción de algunos registros de ejemplo en la tabla de calificaciones */
+INSERT INTO a_Calificaciones (CalificacionID, AlumnoID, ExamenID, Calificacion) VALUES
+(1, 1, 1, 8),
+(2, 1, 2, 7),
+(3, 2, 1, 9),
+(4, 2, 2, 6),
+(5, 3, 1, 7),
+(6, 3, 2, 8),
+(7, 4, 1, 6),
+(8, 4, 2, 5),
+(9, 5, 1, 10),
+(10, 5, 2, 9),
+(11, 6, 1, 8),
+(12, 6, 2, 7);
+
+/* Creación de la tabla de ausencias */
+CREATE TABLE a_Ausencias (
+  AusenciaID INT PRIMARY KEY,
+  AlumnoID INT,
+  Fecha DATE,
+  FOREIGN KEY (AlumnoID) REFERENCES a_Alumnos(AlumnoID)
+);
+
+/* Inserción de algunos registros de ejemplo en la tabla de ausencias */
+INSERT INTO a_Ausencias (AusenciaID, AlumnoID, Fecha) VALUES
+(1, 1, '2022-01-05'),
+(2, 1, '2022-01-10'),
+(3, 2, '2022-01-02'),
+(4, 3, '2022-01-07'),
+(5, 4, '2022-01-10');
+
+/* CONSULTAS */
+
 /* 1.- Nombre de los alumnos que tengan más de 7.5 de promedio en los exámenes */
 SELECT a.Nombre, AVG(c.Calificacion) AS Promedio FROM a_Alumnos a INNER JOIN a_Calificaciones c USING(AlumnoID) GROUP BY AlumnoID HAVING Promedio > 7.5 ORDER BY Promedio;
 SELECT a.Nombre FROM a_Alumnos a INNER JOIN a_Calificaciones c USING(AlumnoID) GROUP BY AlumnoID HAVING AVG(c.Calificacion) > 7.5 ORDER BY AVG(c.Calificacion);
--- Querys/Consultas_BDEjemplos.sql
+++ Querys/Consultas_BDEjemplos.sql
@@ -1,8 +1,100 @@
-SET GLOBAL log_bin_trust_function_creators = 1; -- Poder agregar funciones o procedimientos.
-SET sql_mode=''; -- Poder realizar consultas sin problemas.
+-- Crear tabla d_pais
+CREATE TABLE d_pais (
+  idPais INT PRIMARY KEY,
+  pais VARCHAR(50) NOT NULL
+);
+
+-- Agregar registros a d_pais
+INSERT INTO d_pais (idPais, pais) VALUES
+  (1, 'México'),
+  (2, 'Estados Unidos'),
+  (3, 'España'),
+  (4, 'Argentina'),
+  (5, 'Chile');
+
+-- Crear tabla d_artistas
+CREATE TABLE d_artistas (
+  idArtista INT PRIMARY KEY,
+  nombre VARCHAR(100) NOT NULL,
+  idPais INT,
+  CONSTRAINT fk_pais FOREIGN KEY (idPais) REFERENCES d_pais(idPais)
+);
+
+-- Agregar registros a d_artistas
+INSERT INTO d_artistas (idArtista, nombre, idPais) VALUES
+  (1, 'Juan Gabriel', 1),
+  (2, 'Michael Jackson', 2),
+  (3, 'Luis Miguel', 1),
+  (4, 'Shakira', 4),
+  (5, 'Alejandro Sanz', 3)
+  (6, 'Bandalos Chinos', 4);
+
+-- Crear tabla d_disqueras
+CREATE TABLE d_disqueras (
+  idDisquera INT PRIMARY KEY,
+  nombre VARCHAR(100) NOT NULL,
+  idPais INT,
+  CONSTRAINT fk_pais FOREIGN KEY (idPais) REFERENCES d_pais(idPais)
+);
+
+-- Agregar registros a d_disqueras
+INSERT INTO d_disqueras (idDisquera, nombre, idPais) VALUES
+  (1, 'Sony Music', 2),
+  (2, 'EMI', 2),
+  (3, 'Universal Music', 1),
+  (4, 'Warner Music', 3),
+  (5, 'BMG', 4);
+
+-- Crear tabla d_discos
+CREATE TABLE d_discos (
+  isbn VARCHAR(20) PRIMARY KEY,
+  titulo VARCHAR(100) NOT NULL,
+  idArtista INT,
+  idDisquera INT,
+  CONSTRAINT fk_artista FOREIGN KEY (idArtista) REFERENCES d_artistas(idArtista),
+  CONSTRAINT fk_disquera FOREIGN KEY (idDisquera) REFERENCES d_disqueras(idDisquera)
+);
+
+-- Agregar registros a d_discos
+INSERT INTO d_discos (isbn, titulo, idArtista, idDisquera) VALUES
+  ('1234567890123', 'Recuerdos II', 1, 3),
+  ('2345678901234', 'Thriller', 2, 1),
+  ('3456789012345', 'Romances', 3, 3),
+  ('4567890123456', 'Fijación oral vol. 1', 4, 1),
+  ('5678901234567', 'Más', 5, 4),
+  ('6789012345678', 'Dónde están los ladrones?', 4, 5),
+  ('7890123456789', 'El alma al aire', 5, 2),
+  ('8901234567890', 'Adentro', 5, 5),
+  ('9012345678901', 'X', 4, 3),
+  ('0123456789012', 'Cómplices', 5, 3),
+  ('1234567890124', 'Mis boleros favoritos', 1, 3),
+  ('2345678901235', 'El Big Blue', 6, 2);
+
+CREATE TABLE d_pistas (
+  idPista INT AUTO_INCREMENT PRIMARY KEY,
+  isbn VARCHAR(13),
+  numeroPista INT,
+  nombre VARCHAR(100),
+  duracion TIME,
+  FOREIGN KEY (isbn) REFERENCES d_discos(isbn)
+);
+
+INSERT INTO d_pistas (idPista, numeroPista, nombre, duracion, isbn) VALUES 
+  (1, 1, 'Canción 1', '3:45', '9012345678901'),
+  (2, 2, 'Canción 2', '4:02', '9012345678901'),
+  (3, 3, 'Canción 3', '3:56', '9012345678901'),
+  (4, 1, 'Canción 1', '4:20', '1234567890124'),
+  (5, 2, 'Canción 2', '3:32', '1234567890124'),
+  (6, 1, 'Canción 1', '4:10', '2345678901235'),
+  (7, 2, 'Canción 2', '3:55', '2345678901235'),
+  (8, 3, 'Canción 3', '4:05', '2345678901235');
+
+
+-- SET GLOBAL log_bin_trust_function_creators = 1; -- Poder agregar funciones o procedimientos.
+-- SET sql_mode=''; -- Poder realizar consultas sin problemas.
 
 -- Artistas con numero de discos. 
-1.- SELECT d_artistas.nombre AS Artista, COUNT(isbn) AS No_Discos  FROM d_artistas LEFT JOIN d_discos USING(idArtista) GROUP BY idArtista;
+SELECT d_artistas.nombre AS Artista, COUNT(isbn) AS No_Discos  FROM d_artistas LEFT JOIN d_discos USING(idArtista) GROUP BY idArtista;
 
 -- Paises con numero de artistas. 
 2.- SELECT p.pais AS Pais, COUNT(idArtista) AS Cuantos FROM d_pais p LEFT JOIN d_artistas USING(idPais) GROUP BY idPais;
